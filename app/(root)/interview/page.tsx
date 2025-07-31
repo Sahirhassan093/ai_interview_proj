5b87f8fa9f354444922105940ec5d407a8a7a261
Sahirhassan093 <k224570@nu.edu.pk>
2025-07-31 16:08:28 +0500

    Added full project files directly to main repo

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agnet from "@/components/Agnet";
+
+
+const page = () => {
+  return (
+    <>
+        <h2>Interview Generation</h2>
+        <Agnet userName='You' userId="user1" type="generate"/>
+    </>
+  )
+}
+
+export default page;
